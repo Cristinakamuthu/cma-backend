c43117b426cde7cab5277bd95fa0f870f87bcb56
Cristina <kamuthucristina@gmail.com>
2025-08-11 20:54:14 +0300

    added the serializer section to avoid repetitive code

---
+++ serializer_mixin.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class SerializerMixin:
+    def to_dict(self, include_relationships=False):
+        """
+        convert SQLAlchemy model to dictionary. If include_relationships=True,also serialize related objects.
+        """
+        data = {}
+        for column in self.__table__.columns:
+            value = getattr(self, column.name)
+            if hasattr(value, 'isoformat'):
+                value = value.isoformat()
+            data[column.name] = value
+
+        if include_relationships:
+            for relationship in self.__mapper__.relationchips:
+                value = getattr(self, relationship.key)
+                if value is not None:
+                    if isinstance(value, list):
+                        data[relationship.key] = [item.to_dict() for item in value]
+                    else:
+                        data[relationship.key] = value.to_dict()
+                    
+        return data
